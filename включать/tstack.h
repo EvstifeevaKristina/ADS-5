425cce89bd16d31a1f30f53f847fd00b65f251e6
EvstifeevaKristina <94647555+EvstifeevaKristina@users.noreply.github.com>
2022-04-17 14:31:52 +0300

    Update tstack.h

--- включать/tstack.h
+++ включать/tstack.h
@@ -2,35 +2,29 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-
+template<typename T, int size>
 class TStack {
- private:
+  private:
     T arr[100];
     int top;
-
  public:
-    TStack(): top(-1) { }
-    T get() {
-    if (!isEmpty())
-      return arr[top];
-    else
-    throw "Empty!";
+    TStack() :top(-1) { }
+    T get() const {
+        return arr[top];
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
     }
     void pop() {
-      if (!isEmpty())
-      top--;
+        if (top >= 0)
+            top--;
     }
-    void push(T value) {
-      if (isFull())
-        throw "Full!";
-      else
-        arr[++top] = value;
-     }
-     bool isEmpty() const {
-       return top == -1;
-     }
-     bool isFull() const {
-       return top == size - 1;
-     }
+    void push(T item) {
+        if (top < size - 1)
+            arr[++top] = item;
+    }
 };
-#ifndef INCLUDE_TSTACK_H_
+#endif  // INCLUDE_TSTACK_H_
